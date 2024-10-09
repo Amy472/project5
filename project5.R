c13f827a77d1fcedc248e2f18b058a822e898d2a
Amy472 <hang.vo@lc.cuny.edu>
2024-10-09 19:50:41 -0400

    Add files via upload

---
+++ project5.R
@@ -0,0 +1,21 @@
+# NA_sales
+mean(vgsales$NA_Sales)
+# 0.2646674
+mean(vgsales$NA_Sales, trim = 0.05)
+# 0.1575683
+median(vgsales$NA_Sales)
+# 0.08
+var(vgsales$NA_Sales)
+# 0.6669712
+sd(vgsales$NA_Sales)
+# 0.816683
+
+# JP_sales
+mean(vgsales$JP_Sales)
+# 0.07778166
+mean(vgsales$JP_Sales, trim = 0.05)
+# 0.03104083
+var(vgsales$JP_Sales)
+# 0.0956607
+sd(vgsales$JP_Sales)
+# 0.3092906
